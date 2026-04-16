58fe8b6440fc955ce5475456314e548e7488051e
Kieran Lelong <lelongkieran@gmail.com>
2026-04-16 23:08:26 +0200

    docker test done | fix : versions and pipeline issue

--- dashboard/app.py
+++ dashboard/app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import requests
 import plotly.graph_objects as go 
 import time
+import os
 
 # Page configuration
 st.set_page_config(
@@ -11,7 +12,7 @@ st.set_page_config(
     layout="wide"
 )
 
-API_URL = "http://127.0.0.1:8000/history"
+API_URL = os.getenv("API_URL", "http://127.0.0.1:8000/history")
 
 def fetch_data():
     try:
